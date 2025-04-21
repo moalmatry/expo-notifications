c175062b8b3beea9884f285d8680d270d6425122
Mohamed Almatry <moalmatry2000@gmail.com>
2025-04-21 13:26:32 +0200

    feat: add push notifications and test it with android

---
+++ hooks/usePushNotifications.ts
@@ -0,0 +1,87 @@
+import { useState, useEffect, useRef } from "react";
+import { Platform } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import constants from "expo-constants";
+
+export interface PushNotificationState {
+  notification?: Notifications.Notification;
+  expoPushToken?: Notifications.ExpoPushToken;
+}
+
+export const usePushNotifications = () => {
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    }),
+  });
+  const [expoPushToken, setExpoPushToken] = useState<
+    Notifications.ExpoPushToken | undefined
+  >();
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >();
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  const registerForPushNotificationsAsync = async () => {
+    let token: Notifications.ExpoPushToken;
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: constants.expoConfig?.extra?.eas.projectId,
+      });
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          sound: "default",
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+      return token;
+    } else {
+      console.error("Push notifications are not supported on simulators");
+    }
+  };
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      setExpoPushToken(token);
+    });
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current!
+      );
+      Notifications.removeNotificationSubscription(responseListener.current!);
+    };
+  }, []);
+
+  return {
+    expoPushToken,
+    notification,
+  } as PushNotificationState;
+};
